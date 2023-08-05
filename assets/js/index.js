acb17bfd469fb25ea758b50695fbf3cb4067ed88
KC_Dev <burakkcelebi@hotmail.com>
2023-08-05 21:48:44 +0100

    scroll up button is added

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,3 +14,24 @@ const toggle = () => {
     burgerIcon.style.display = "none";
   }
 };
+
+const calcScrollValue = () => {
+  let scrollProgress = document.querySelector(".progress");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 120) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#7843e9 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;

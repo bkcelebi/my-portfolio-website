6d333135472c743e39ac23c1edb68792290761fe
KC_Dev <burakkcelebi@hotmail.com>
2023-08-09 16:08:53 +0100

    sidebar touch adjusted

--- assets/js/index.js
+++ assets/js/index.js
@@ -37,8 +37,8 @@ const collapse = () => {
   sidebarLinks.style.padding = "1rem 0.5rem";
 };
 
-const html = document.querySelector("html");
-html.addEventListener("click", () => {
+const body = document.querySelector("body");
+body.addEventListener("click", () => {
   collapse();
 });
 
